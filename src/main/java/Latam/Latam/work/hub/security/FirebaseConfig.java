424c8b1f868a475663fadc82346bcf10b54c2a05
405516_Gonzalez_Federico <164792276+405516GONZALEZFEDERICO@users.noreply.github.com>
2025-04-17 16:55:45 -0300

    change

--- src/main/java/Latam/Latam/work/hub/security/FirebaseConfig.java
+++ src/main/java/Latam/Latam/work/hub/security/FirebaseConfig.java
@@ -12,7 +12,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void initFirebase() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/firebase-config.json");
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-config.json");
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
