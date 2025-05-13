ab9585cb8864247f2e06d51955028bba4c41a2d1
FedeGonzalez <164792276+405516GONZALEZFEDERICO@users.noreply.github.com>
2025-05-13 12:28:17 -0300

    Update BookingEntity.java

--- src/main/java/Latam/Latam/work/hub/entities/BookingEntity.java
+++ src/main/java/Latam/Latam/work/hub/entities/BookingEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -57,13 +58,13 @@ public class BookingEntity implements Billable {
 
 
     @Column(name = "total_amount")
-    private Double totalAmount;           
+    private Double totalAmount;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "space_id")
-    private SpaceEntity space;          
+    private SpaceEntity space;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
